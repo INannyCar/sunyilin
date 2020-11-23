32f2cc02852f088c7a5778f9e1d716ebfa359215
name <email>
2020-11-23 13:02:46 +0900

    11.23 2
    11.23 2

--- final/app/src/main/java/com/cookandroid/afinal/lastActivity.java
+++ final/app/src/main/java/com/cookandroid/afinal/lastActivity.java
@@ -1,7 +1,9 @@
 package com.cookandroid.afinal;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
@@ -12,10 +14,9 @@ import static com.cookandroid.afinal.R.drawable.wendu;
 import static com.cookandroid.afinal.R.drawable.wenduliang;
 
 public class lastActivity extends AppCompatActivity {
-    private Button btn_LED,btn_blue;
-    private ImageView tv,bl;
+    private Button btn_LED;
+    private ImageView tv;
     private boolean status=false;
-    private boolean statusw=false;
     private ImageView show;
     @Override
 
@@ -24,23 +25,9 @@ public class lastActivity extends AppCompatActivity {
         setContentView(R.layout.activity_last);
         show = findViewById(R.id.wen);
         btn_LED = (Button) findViewById(R.id.btnLed);
-//        btn_blue = (Button) findViewById(R.id.tooth);
+
         tv=findViewById(R.id.dengl);
-//        bl=findViewById(R.id.blue);
-//        btn_blue.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                if(!statusw){
-//
-//                    bl.setBackgroundResource(R.drawable.lanbu);
-//                    statusw=true;
-//                }else{
-//
-//                    bl.setBackgroundResource(R.drawable.lan);
-//                    statusw=false;
-//                }
-//            }
-//        });
+
         btn_LED.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -60,11 +47,37 @@ public class lastActivity extends AppCompatActivity {
     }
     public void click(View View){
         show.setImageDrawable(getResources().getDrawable((wenduliang)));
-//        searchIv.setBackgroundResource(R.drawable.search2);
         blink();
 
 
     }
+    public void showdialog(View view)
+    {
+
+        AlertDialog.Builder alertdialogbuilder=new AlertDialog.Builder(this);
+        alertdialogbuilder.setMessage("종료하시겠습니까?");
+        alertdialogbuilder.setPositiveButton("확인",click1);
+        alertdialogbuilder.setNegativeButton("취소",click2);
+        AlertDialog alertdialog1=alertdialogbuilder.create();
+        alertdialog1.show();
+     }
+
+     private DialogInterface.OnClickListener click1=new DialogInterface.OnClickListener(){
+
+    @Override
+     public void onClick(DialogInterface arg0,int arg1)
+    {
+        android.os.Process.killProcess(android.os.Process.myPid());
+    }
+    };
+    private DialogInterface.OnClickListener click2=new DialogInterface.OnClickListener()
+    {
+        @Override
+        public void onClick(DialogInterface arg0,int arg1)
+        {
+            arg0.cancel();
+        }
+    };
     private void blink(){
         final Handler handler = new Handler();
         new Thread(new Runnable() {
@@ -83,7 +96,6 @@ public class lastActivity extends AppCompatActivity {
                             }else{
                                 txt.setVisibility(View.VISIBLE);
                             }
-//                    blink();
                         }
                     });
                     i++;
